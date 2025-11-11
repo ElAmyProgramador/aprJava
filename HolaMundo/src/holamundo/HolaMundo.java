516c3dd7899b794356c5774a0a8283c8a91fda77
Psykokwak <alexeivra@gmail.com>
2025-11-11 00:16:12 -0600

    Empezamos a aprender Java, desde netbeans 27

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,9 @@
+package holamundo;
+
+// simplemente un hola mundo de toda la vida
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, estoy aprendiendo Java :/");
+    }
+}
