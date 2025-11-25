62e937ad91315b9e93491c3cacfde2b29d3e3bc4
Psykokwak <alexeivra@gmail.com>
2025-11-25 00:40:26 -0600

    Ejercicio de encontrar el corredor mas rapido

--- Poo/src/Ejercicios/Principal.java
+++ Poo/src/Ejercicios/Principal.java
@@ -3,6 +3,7 @@ package Ejercicios;
 import java.util.Scanner;
 
 public class Principal {
+    /*
     public static int indiceCocheBarato(Coche autos[]) {
         float precio;
         int indice = 0;
@@ -10,6 +11,18 @@ public class Principal {
         for (int i = 1; i < autos.length; i++) {
             if (autos[i].getPrecio() < precio) {
                 precio = autos[i].getPrecio();
+                indice = i;
+            }
+        }
+        return indice;
+    } */
+    
+    public static int indiceGanador(Corredor lista[]) {
+        double tiempo = lista[0].getTiempoCarrera();
+        int indice = 0;
+        for(int i = 1; i < lista.length; i++) {
+            if (lista[i].getTiempoCarrera() < tiempo) {
+                tiempo = lista[i - 1].getTiempoCarrera();
                 indice = i;
             }
         }
@@ -31,6 +44,8 @@ public class Principal {
         }
         System.out.println("El perimetro es " + c1.getPerimetro() + " y el area es " + c1.getArea());
         */
+        
+        /*
         String marca, modelo;
         float precio;
         System.out.println("Cantidad de vehiculos? ");
@@ -48,5 +63,26 @@ public class Principal {
         }
         int indiceBarato = indiceCocheBarato(coches);
         System.out.println("El coche mas barato es " + coches[indiceBarato].mostrarDatos());
+        */
+        
+        String nombre;
+        int numId;
+        double tiempo;
+        System.out.println("¿Cuántos corredores competiran?");
+        int cantidadCorredores = teclado.nextInt();
+        Corredor corredores[] = new Corredor[cantidadCorredores];
+        for (int i = 0; i < cantidadCorredores; i++) {
+            System.out.println("Información del corredor " + (i + 1));
+            teclado.nextLine();
+            System.out.println("Nombre del corredor: ");
+            nombre = teclado.nextLine();
+            System.out.println("ID del corredor: ");
+            numId = teclado.nextInt();
+            System.out.println("Tiempo que realizo el corredor: ");
+            tiempo = teclado.nextDouble();
+            corredores[i] = new Corredor(nombre, numId, tiempo);
+        }
+        int ganador = indiceGanador(corredores);
+        System.out.println("Ganó el corredor:\n" + corredores[ganador].mostrarCorredor());
     }
 }
