164ee8166b8eaf28e3c815544494ac2d1c9e1a7e
Psykokwak <alexeivra@gmail.com>
2025-11-29 11:28:22 -0600

    remplazo de setSize y setLocation con setBounds

--- Ventana/src/ventana/Ventana.java
+++ Ventana/src/ventana/Ventana.java
@@ -4,9 +4,8 @@ import javax.swing.JFrame;
 
 public class Ventana extends JFrame {
     public Ventana() {
-        this.setSize(900, 600);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Mi primer ventana");
-        this.setLocation(450, 80);
+        this.setBounds(300, 80, 900, 600);
     }
 }
