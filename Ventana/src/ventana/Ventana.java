71f2940f65bbd7cab26fb90abbfb77e1e353c387
Psykokwak <alexeivra@gmail.com>
2025-11-29 11:35:58 -0600

    poner la ventana en el centro

--- Ventana/src/ventana/Ventana.java
+++ Ventana/src/ventana/Ventana.java
@@ -6,6 +6,8 @@ public class Ventana extends JFrame {
     public Ventana() {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Mi primer ventana");
-        this.setBounds(300, 80, 900, 600);
+        // this.setBounds(300, 80, 900, 600); // establecer coordenadas y tamaño
+        this.setSize(900, 600);
+        this.setLocationRelativeTo(null); // pantalla en el centro
     }
 }
